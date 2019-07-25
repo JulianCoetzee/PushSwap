1b191ea80e01c64744113ed4e55eb802bd863ef0
Julian COETZEE <jcoetzee@c5r2s4.wethinkcode.co.za>
2019-07-25 13:02:32 +0200

    Header update

--- push_swap.h
+++ push_swap.h
@@ -6,8 +6,30 @@
 /*   By: jcoetzee <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/25 11:59:40 by jcoetzee          #+#    #+#             */
-/*   Updated: 2019/07/25 11:59:43 by jcoetzee         ###   ########.fr       */
+/*   Updated: 2019/07/25 13:01:10 by jcoetzee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+typedef struct s_stacks
+{
+	int		*stack_a;
+	int		*stack_b;
+	int		size_a;
+	int		size_b;
+}			t_stacks;
+
+typedef	struct	s_moves
+{
+	int		a_moves;
+	int		b_moves;
+	int		c_moves;
+	int		total;
+	int		elem;
+	char	rot_a;
+	char	rot_b;
+	char	rot_c;
+}			t_moves;
 
